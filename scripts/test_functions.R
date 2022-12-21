5f2fa6ceba177d4aee6f3e412dd01f589187e177
teobucci <teobucci8@gmail.com>
2022-12-21 11:05:46 +0100

    Implement get S from old S, oldrho, rho, G

--- scripts/test_functions.R
+++ scripts/test_functions.R
@@ -127,6 +127,126 @@ get_S_from_G_rho = function(G, rho) {
     # TODO fare questo per evitare di bruciare risorse, già che non ce ne sono troppe
 }
 
+# faccio un tentativo di scrivere sta roba senza ricalcolare tutto
+# ma partendo da G, vecchia rho e sua S, nuova rho
+
+get_S_from_G_rho_oldrho_oldS = function(G,rho,oldrho,oldS,debug=F){
+    # check on G
+    if (!all(t(G) == G))
+        stop("G is not symmetric")
+    
+    M = length(rho) # number of groups in new rho
+    oldM = length(oldrho) # number of groups in old rho
+    S = matrix(numeric(M * M), nrow = M, byrow = T) # initialize S matrix
+    bounds = cumsum(rho) # index of the right bounds of the partition
+    
+    groups_to_be_refilled = {}
+    
+    if(M > oldM){
+        if(debug)
+            cat("Entering case ADD\n")
+        # caso di add
+        
+        # find the group that has changed
+        # notazione infelice perche' lo chiamiamo anche lui S, va uniformato
+        K = min(which(rho != c(oldrho,NA)))
+        
+        if(debug)
+            cat("Group affected K =",K,"\n")
+        
+        if(K > 1 && K < oldM){ # in the standard case perform all four
+            S[1:(K-1),1:(K-1)] = oldS[1:(K-1),1:(K-1)] # upper left block
+            S[(K+1+1):M,1:(K-1)] = oldS[(K+1):oldM,1:(K-1)] # lower left block
+            S[1:(K-1),(K+1+1):M] = oldS[1:(K-1),(K+1):oldM] # upper right block
+            S[(K+1+1):M,(K+1+1):M] = oldS[(K+1):oldM,(K+1):oldM] # lower right block
+        } else if(K == 1){ # bring to the new S only the lower right block
+            S[(K+1+1):M,(K+1+1):M] = oldS[(K+1):oldM,(K+1):oldM] # lower right block
+        } else if(K == oldM){ # bring to the new S only the upper left block
+            S[1:(K-1),1:(K-1)] = oldS[1:(K-1),1:(K-1)] # upper left block
+        }
+        
+        
+        groups_to_be_refilled = c(K,K+1)
+        
+    } else if(M < oldM) {
+        if(debug)
+            cat("Entering case DELETE\n")
+        # caso di delete
+        
+        # find the group that has changed
+        # notazione infelice perche' lo chiamiamo anche lui S, va uniformato
+        K = min(which(oldrho != c(rho,NA)))
+        
+        if(debug)
+            cat("Group affected K =",K,"\n")
+        
+        if(K > 1 && K+1 < oldM){ # in the standard case perform all four
+            S[1:(K-1),1:(K-1)] = oldS[1:(K-1),1:(K-1)] # upper left block
+            S[(K+1):M,1:(K-1)] = oldS[(K+1+1):oldM,1:(K-1)] # lower left block
+            S[1:(K-1),(K+1):M] = oldS[1:(K-1),(K+1+1):oldM] # upper right block
+            S[(K+1):M,(K+1):M] = oldS[(K+1+1):oldM,(K+1+1):oldM] # lower right block
+        } else if(K == 1){ # bring to the new S only the lower right block
+            S[(K+1):M,(K+1):M] = oldS[(K+1+1):oldM,(K+1+1):oldM] # lower right block
+        } else if(K+1 == oldM){ # bring to the new S only the upper left block
+            S[1:(K-1),1:(K-1)] = oldS[1:(K-1),1:(K-1)] # upper left block
+        }
+        
+        groups_to_be_refilled = c(K)
+        
+    } else {
+        if(debug)
+            cat("Entering case SHUFFLE\n")
+        # caso di shuffle quando sono uguali il numero di partizioni
+        
+        S = oldS
+        
+        # find the group that has changed
+        # notazione infelice perche' lo chiamiamo anche lui S, va uniformato
+        K = min(which(oldrho != rho))
+        
+        if(debug)
+            cat("Group affected K =",K,"\n")
+        
+        S[K:(K+1),1:M] = 0 # row K to K+1
+        S[1:M,K:(K+1)] = 0 # column K to K+1
+        
+        groups_to_be_refilled = c(K,K+1)
+        
+    }
+
+    # sistemare i loop qui sotto solo dove serve
+    cat("Matrix S before filling\n")
+    print(S)
+    
+    # loop through the groups
+    for (l in groups_to_be_refilled) {
+        # cat("analysing group", i, "\n")
+        # extract the submatrix and sum all the elements
+        # the inside connections are now counted twice
+        for (m in 1:M) {
+            start_row = ifelse(m != 1, bounds[m - 1] + 1, 0)
+            end_row = bounds[m]
+            start_col = ifelse(l != 1, bounds[l - 1] + 1, 0)
+            end_col = bounds[l]
+            S[l, m] = sum(G[start_row:end_row, start_col:end_col])
+            S[m, l] = S[l, m]
+            if(l == m)
+                S[l, m] = S[l, m] / 2 # correct the diagonal
+            if(debug)
+                cat("connection between", l, "and", m, "=", S[l, m], "\n")
+        }
+    }
+    
+    
+    
+    # make symmetric
+    # S = makeSymm(S,from_lower=F)
+    # ho problemi a capire se devo fare dalla lower o dalla upper, risolvo inserendo sopra e amen
+    
+    return(S)
+    
+}
+
 
 G = matrix(c(
     0,0,1,0,1,
@@ -136,6 +256,16 @@ G = matrix(c(
     1,0,0,1,0
 ),nrow=5,byrow = T)
 
-rho = c(1, 3, 1)
+oldrho = c(1,3,1)
+rho = c(1,2,1,1)
+oldS = get_S_from_G_rho(G=G,rho=oldrho)
+S_method_correct = get_S_from_G_rho(G=G,rho=rho)
+S_new_method = get_S_from_G_rho_oldrho_oldS(G,rho,oldrho,oldS,debug=T)
 
-get_S_from_G_rho(G=G,rho=rho)
+if(all(S_new_method == S_method_correct)){
+    cat("Ce l'hai fatta Jonny")
+}
+
+
+
+
